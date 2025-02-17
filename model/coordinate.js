d7eaefed0dcfc20162a1bd895c9c9b6cf6e651fb
BetterThanYou73 <manas2005mj@gmail.com>
2025-02-16 20:19:13 -0700

    fixed homepage.html

--- model/coordinate.js
+++ model/coordinate.js
@@ -1,21 +1,22 @@
-
-var locations = []
-fetch("http://127.0.0.1:5000/process", {
+fetch("https://myuser.pythonanywhere.com/process", {
     method: "POST",
     headers: {
-        "Content-Type": "application/json"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-        elements: window.inputElements  // Use inputs from initial input file
+      elements: window.inputElements // Use inputs from your page
     })
-})
-.then(response => response.json())
-.then(data => {
-    console.log("Received data from Flask:", data);
-
-    // Directly assign API response to locations
-    locations = data;  })
-for (let i = 0; i < locations.length; i++) {
-    let [lat, lon] = locations[i]; 
-    console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Received data from Flask:", data);
+  
+      // Now that data is received, loop over the locations array.
+      // Make sure the loop is inside the .then() callback.
+      const locations = data;
+      locations.forEach(([lat, lon]) => {
+        console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+      });
+    })
+    .catch(error => console.error("Error:", error));
 }
